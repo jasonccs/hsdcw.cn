f94ff14fbc586ccc33facccb88b138eb1029277b
jasonccs-home <1521910992@qq.com>
2018-08-05 19:32:54 +0800

    k

--- Application/Home/Widget/AboutArticleWidget.class.php
+++ Application/Home/Widget/AboutArticleWidget.class.php
@@ -9,7 +9,7 @@
         public function about()
         {
             $about=D('News')->field('id,title,cateid,keywords,tag,source,desc_image,description,time')->relation(true)->order('eye desc')->limit(4)->select();
-            dump($about);
+//            dump($about);
             $this->assign('url',__ACTION__.'/');
             $this->assign('about',$about);
             $this->display('Widget:about');
