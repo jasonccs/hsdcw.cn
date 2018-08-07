68518deae3d95efa0ef9ee785b2858f96964be50
jasonccs-company <1521910992@qq.com>
2018-08-07 12:20:41 +0800

    k

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -3,7 +3,7 @@ namespace Admin\Controller;
 use  Think\Controller;
 
 /**
-* 
+*
 */
 
 class CommonController extends Controller
@@ -15,16 +15,16 @@ class CommonController extends Controller
 	//自动运行的方法
 	public function _initialize()
 	{
-		
+
 		if(!isset($_SESSION['username'])||!isset($_SESSION['admin_id'])){
-			
-		    $this->error('你还有登录，请先登录！',U('Admin/Login/index'),'2');
+
+		    $this->error('你还有登录，请先登录！',U('/Admin/Login/index'),'2');
 		}
 	}
 
 	//删除文件的方法
 	public function delImg(){
-			$imgpath=I('delimgpath',null); 
+			$imgpath=I('delimgpath',null);
 			if(IS_AJAX){
 				if(unlink('./'.$imgpath)){
 					 $data=array('status'=>true);
--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -2,13 +2,13 @@
 namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
-   
+
    /**
     * 显示登录页
     */
-    
+
     public function index(){
-    
+
        $this->display('login');
     }
 
@@ -21,8 +21,8 @@ class LoginController extends Controller {
     	  if(IS_POST){
         	$db=M('user');
 	      	$username =I('username',null);
-	      	$password =I('password',null); 
-	      	$code =I('code',null); 
+	      	$password =I('password',null);
+	      	$code =I('code',null);
 	        $username_info=$db->where(array('username'=>$username))->find();
 	        if(is_null($username_info)){
 	        	$this->error('用户名不存在！');
@@ -33,12 +33,12 @@ class LoginController extends Controller {
 	        }else{
                 session('admin_id',$username_info['id']);
                 session('username',$username_info['username']);
-	        	$this->success('登录成功',U('Admin/Index/index'));
+	        	$this->success('登录成功',U('/Admin/Index/index'));
 
 	        }
-	       
+
         }
-      	
+
 
 
     }
@@ -63,7 +63,7 @@ class LoginController extends Controller {
 		    ob_end_clean();//清除缓存
 	        //C('verify'); //读取Common/conf/config.php验证码的配置
 	    	$verify=new \Think\Verify(C('verify'));
-	        $verify->entry();  
+	        $verify->entry();
 	}
 
 
