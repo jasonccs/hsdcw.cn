993be5714897578d160c3323757b7a4813119e16
jasonccs-company <1521910992@qq.com>
2018-08-07 10:55:52 +0800

    k

--- Application/Home/View/Public/js/fangchan_slider.js
+++ Application/Home/View/Public/js/fangchan_slider.js
@@ -1,63 +1,63 @@
-jQuery.fn.extend({
-	
-	slideFocus: function(){
-		var This = $(this);
-		var sWidth = $(This).width(),
-			len    =$(This).find('.fangchan_focus ul').length,
-			index  = 0,
-			Timer;
-  // alert(len);
-		// btn event
-		var btn = "<div class='btn'>";
-		for(var i=0; i < len; i++) {
-			btn += "<span></span>";
-		};
-		btn += "</div><div class='preNext pre'></div><div class='preNext next'></div>";
-		$(This).append(btn);
-		$(This).find('.btn span').eq(0).addClass('on');
+$(function () {
+    $.fn.extend({
+        slideFocus: function(){
+            let This = $(this);
+            let sWidth = $(This).width(),
+                len    =$(This).find('.fangchan_focus ul').length,
+                index  = 0,
+                Timer;
+            // alert(len);
+            let btn = "<div class='btn'>";
+            for(let i=0; i < len; i++) {
+                btn += "<span></span>";
+            };
+            btn += "</div><div class='preNext pre'></div><div class='preNext next'></div>";
+            $(This).append(btn);
+            $(This).find('.btn span').eq(0).addClass('on');
 
 
-		$(This).find('.btn span').mouseover(function(){
-			index = $(This).find('.btn span').index(this);
-			Tony(index);
-		});
+            $(This).find('.btn span').mouseover(function(){
+                index = $(This).find('.btn span').index(this);
+                Tony(index);
+            });
 
-		$(This).find('.next').click(function(){
-			index++;
-			if(index == len){index = 0;}
-			Tony(index);
-		});
+            $(This).find('.next').click(function(){
+                index++;
+                if(index == len){index = 0;}
+                Tony(index);
+            });
 
-		$(This).find('.pre').click(function(){
-			index--;
-			if(index == -1){index = len - 1;}
-			Tony(index);
-		});
+            $(This).find('.pre').click(function(){
+                index--;
+                if(index == -1){index = len - 1;}
+                Tony(index);
+            });
 
 
-		// start setInterval		
-		$(This).find('.fangchan_focus').css("width",sWidth * (len));
-		$(This).hover(function(){
-			clearInterval(Timer);
-			show($(This).find('.preNext'));
-		},function(){
-			hide($(This).find('.preNext'));
-			Timer=setInterval(function(){
-				Tony(index);
-				index++;
-				if(len == index){index = 0;}
-			}, 2000)
-		}).trigger("mouseleave");
+            // start setInterval
+            $(This).find('.fangchan_focus').css("width",sWidth * (len));
+            $(This).hover(function(){
+                clearInterval(Timer);
+                show($(This).find('.preNext'));
+            },function(){
+                hide($(This).find('.preNext'));
+                Timer=setInterval(function(){
+                    Tony(index);
+                    index++;
+                    if(len == index){index = 0;}
+                }, 2000)
+            }).trigger("mouseleave");
 
-		function Tony(index){
-			var new_width = -index * sWidth;
-			$(This).find('.fangchan_focus').stop(true,false).animate({'left' : new_width},300);
-			$(This).find('.btn span').stop(true,false).eq(index).addClass('on').siblings().removeClass('on');
-		};
+            function Tony(index){
+                let new_width = -index * sWidth;
+                $(This).find('.fangchan_focus').stop(true,false).animate({'left' : new_width},300);
+                $(This).find('.btn span').stop(true,false).eq(index).addClass('on').siblings().removeClass('on');
+            };
 
-		
-		// show hide
-		function show(obj){ $(obj).stop(true,false).fadeIn();}
-		function hide(obj){ $(obj).stop(true,false).fadeOut();}
-	}
+
+            // show hide
+            function show(obj){ $(obj).stop(true,false).fadeIn();}
+            function hide(obj){ $(obj).stop(true,false).fadeOut();}
+        }
+    });
 });
