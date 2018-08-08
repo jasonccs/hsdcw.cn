61d2d56db6e02a2cb33210feb5c0b55808d32928
jasonccs-company <1521910992@qq.com>
2018-08-08 17:30:31 +0800

    k

--- Application/Member/Conf/config.php
+++ Application/Member/Conf/config.php
@@ -6,7 +6,9 @@ return [
     'TMPL_PARSE_STRING' => array(
 
         '__PUBLIC__' => __ROOT__ . '/Application/Member/View/Public', // 更改默认的/Public 替换规则
-        '__STATIC__' => '/Public', // 增加新的JS类库路径替换规则
+        '__CSS__' => '/Public/css', // 增加新的JS类库路径替换规则
+        '__JS__' => '/Public/js', // 增加新的JS类库路径替换规则
+        '__IMAGES__' => '/Public/images', // 增加新的JS类库路径替换规则
     ),
 
 
