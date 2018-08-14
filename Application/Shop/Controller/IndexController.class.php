32a2aaa244422e3ae452139bf027f3d10bcb9c5e
jasonccs-home <1521910992@qq.com>
2018-08-14 22:34:30 +0800

    model-shop

---
+++ Application/Shop/Controller/IndexController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shop\Controller;
+
+use Common\Controller\CommonController;
+
+class IndexController extends CommonController
+{
+    
+
+    
+    public function index ()
+    {
+    
+        $this->display();
+    
+    }
+}
