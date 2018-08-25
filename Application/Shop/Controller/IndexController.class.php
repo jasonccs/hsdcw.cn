fb879ff7aac2f064e01899b79299042db2096942
jasonccs-home <1521910992@qq.com>
2018-08-25 23:20:43 +0800

    购车

--- Application/Shop/Controller/IndexController.class.php
+++ Application/Shop/Controller/IndexController.class.php
@@ -15,4 +15,19 @@ class IndexController extends CommonController
         $this->display();
     
     }
+    
+    //购物车页面
+    public function cart(){
+        
+        $this->display();
+        
+    }
+    
+    public function cartEdit(){
+        $cartType=I('cartType',1);
+        $cartType=I('num',1);
+        $skuId=I('skuId',5086);
+        $this->ajaxReturn(['retCode'=>200,'retMsg'=>'编辑成功']);
+    }
+    
 }
